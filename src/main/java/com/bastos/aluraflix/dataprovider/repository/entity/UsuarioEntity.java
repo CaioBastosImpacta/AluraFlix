55cdfc32c6da709fa81a53f343d53e2dc5a044dd
CaioBastosImpacta <caio.bastos@aluno.faculdadeimpacta.com.br>
2021-08-08 19:45:56 -0300

    alterando propriedade para subir em prod

--- src/main/java/com/bastos/aluraflix/dataprovider/repository/entity/UsuarioEntity.java
+++ src/main/java/com/bastos/aluraflix/dataprovider/repository/entity/UsuarioEntity.java
@@ -28,6 +28,8 @@ public class UsuarioEntity implements UserDetails {
     private String senha;
 
     @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "usuario_perfil", joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "perfil_id"))
     private List<PerfilEntity> perfis = new ArrayList<>();
 
     @Override
